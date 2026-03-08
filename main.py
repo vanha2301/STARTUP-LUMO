bd17a4b7a0a8de83ad9b5b480db277387e8a64a3
Van Ha Tran <vanha.cv.vn@gmail.com>
2026-03-08 20:16:10 +0000

    test server Tyranno

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {
+        "status": "Success",
+        "message": "Server Lumo running, create by Ha V. Tran (Tyranno)",
+        "project": "Lumo Hub - STARTUP CHALLENGE 2026",
+        "author": "Ha V. Tran (Tyranno)"
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7001)
